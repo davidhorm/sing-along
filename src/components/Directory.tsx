cb50a490f5c31230074ab7a8e4417376a6c573d8
davidhorm <davidhorm+github@gmail.com>
2020-12-13 12:03:35 -0800

    Add close-captioned songs

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Directory.css';
-import { SongMetadata } from '../App';
 import ClosedCaptionOutlinedIcon from '@material-ui/icons/ClosedCaptionOutlined';
+import { SongMetadata } from '../App';
 
 type DirectoryProps = {
     songList: SongMetadata[];
@@ -11,17 +11,19 @@ export const Directory = ({songList, setSelectedSong}: DirectoryProps) => {
     return (<>
         <h1>🎄🎅🤶🎵 Christmas Sing Along 🎶☃️🎁🎄</h1>
         <section className="directory">
-            {songList.map((songMetadata: SongMetadata) => {
-                const title = songMetadata.lyricsFileName.replace('.lrc', '');
-                const youtubeThumbnailSrc = `https://img.youtube.com/vi/${songMetadata.videoId}/mqdefault.jpg`;
+            {songList
+                .sort((a: SongMetadata, b: SongMetadata) => a.lyricsFileName.localeCompare(b.lyricsFileName))
+                .map((songMetadata: SongMetadata) => {
+                    const [songTitle, artist] = songMetadata.lyricsFileName.replace('.lrc', '').split(' - ');
+                    const youtubeThumbnailSrc = `https://img.youtube.com/vi/${songMetadata.videoId}/mqdefault.jpg`;
 
-                return <button key={songMetadata.videoId} onClick={() => setSelectedSong(songMetadata)}>
-                    <img src={youtubeThumbnailSrc} alt={`Youtube Thumbnail of ${title}`} />
-                    <div>
-                        {title}
-                        {songMetadata.cc && <ClosedCaptionOutlinedIcon className="cc-icon" />}
-                    </div>
-                </button>
+                    return <button key={songMetadata.videoId} onClick={() => setSelectedSong(songMetadata)}>
+                        <img src={youtubeThumbnailSrc} alt={`Youtube Thumbnail of ${songTitle}`} />
+                        <div>
+                            {songTitle} <br /> {artist}
+                            {songMetadata.cc && <ClosedCaptionOutlinedIcon className="cc-icon" />}
+                        </div>
+                    </button>
                 }
             )}
         </section>
