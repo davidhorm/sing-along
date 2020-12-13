1e24d94694b1a22eea156b060bf29cfed5430ffa
davidhorm <davidhorm+github@gmail.com>
2020-12-13 09:40:03 -0800

    Renamed .lrc to be title first

--- src/components/playlist.ts
+++ src/components/playlist.ts
@@ -1,4 +1,4 @@
 export const playlist = [
-    {videoId: 'aAkMkVFwAoo', lyricsUrl: '/lyrics/Mariah Carey - All I Want for Christmas Is You.lrc'},
-    {videoId: 'k4s1wSCvs8k', lyricsUrl: '/lyrics/Boyz II Men ft. Brian McKnight- Let It Snow.lrc'},
+    {videoId: 'aAkMkVFwAoo', lyricsUrl: '/lyrics/All I Want for Christmas Is You - Mariah Carey.lrc'},
+    {videoId: 'k4s1wSCvs8k', lyricsUrl: '/lyrics/Let It Snow - Boyz II Men ft. Brian McKnight.lrc'},
 ];
