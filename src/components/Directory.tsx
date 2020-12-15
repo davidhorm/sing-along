6e008ac1a30dd28e5253ce3b39e61c3f268d03a5
davidhorm <davidhorm+github@gmail.com>
2020-12-15 14:59:10 -0800

    Use Material button and styles

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import './Directory.css';
 import ClosedCaptionOutlinedIcon from '@material-ui/icons/ClosedCaptionOutlined';
 import { SongMetadata } from '../App';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+    label: { 
+        flexDirection: 'column',
+        minWidth: '420px'
+    },
+});
 
 type DirectoryProps = {
     songList: SongMetadata[];
     setSelectedSong: Function;
 }
 export const Directory = ({songList, setSelectedSong}: DirectoryProps) => {
+    const classes = useStyles();
+
     return (<>
         <h1>🎄🎅🤶🎵 Christmas Sing Along 🎶☃️🎁🎄</h1>
         <section className="directory">
@@ -17,13 +28,18 @@ export const Directory = ({songList, setSelectedSong}: DirectoryProps) => {
                     const [songTitle, artist] = songMetadata.lyricsFileName.replace('.lrc', '').split(' - ');
                     const youtubeThumbnailSrc = `https://img.youtube.com/vi/${songMetadata.videoId}/mqdefault.jpg`;
 
-                    return <button key={songMetadata.videoId} onClick={() => setSelectedSong(songMetadata)}>
-                        <img src={youtubeThumbnailSrc} alt={`Youtube Thumbnail of ${songTitle}`} />
-                        <div>
-                            {songTitle} <br /> {artist}
-                            {songMetadata.cc && <ClosedCaptionOutlinedIcon className="cc-icon" />}
-                        </div>
-                    </button>
+                    return (
+                        <Button 
+                            variant="outlined"
+                            classes={{ label: classes.label }}
+                            key={songMetadata.videoId}
+                            onClick={() => setSelectedSong(songMetadata)}>
+                            <img src={youtubeThumbnailSrc} alt={`Youtube Thumbnail of ${songTitle}`} />
+                            <div>
+                                {songTitle} <br /> {artist}
+                                {songMetadata.cc && <ClosedCaptionOutlinedIcon className="cc-icon" />}
+                            </div>
+                        </Button>);
                 }
             )}
         </section>
