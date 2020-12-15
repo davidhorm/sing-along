9c7f041bde2e8ffdc68f6334c8a7777848db4e7f
davidhorm <davidhorm+github@gmail.com>
2020-12-14 23:00:22 -0800

    Update <SyncLyrics /> for long lyrics

--- src/components/SyncLyrics.tsx
+++ src/components/SyncLyrics.tsx
@@ -10,19 +10,20 @@ export const SyncLyrics = ({ videoId }: SyncLyricsProps) => {
     const { milliseconds: youTubeTime } = useYouTube({videoId});
 
     return (
-        <section className="sync-lyrics">
-            <div id={videoId}></div>
-
+        <section className="sync-lyrics" style={{display: 'flex'}}>
             <div>
-                <textarea onChange={e => setLyricLines(e.target.value.split('\n'))}></textarea>
-                <button onClick={() => setMilliseconds( current => [ ...current, youTubeTime ])}>SET TIME</button>
+                <div id={videoId}></div><br />
+                <textarea onChange={e => setLyricLines(e.target.value.split('\n'))}></textarea><br />
+                <button onClick={() => setMilliseconds( current => [ ...current, youTubeTime ])}>SET TIME</button><br />
                 <button onClick={() => setMilliseconds( array => array.filter((_, index) => index !== milliseconds.length - 1))}>UNDO</button>
             </div>
             
-            {lyricLines.map((lyric, index) => {
-                const timestamp = milliseconds[index] ? `[${new Date(0, 0, 0, 0, 0, 0, milliseconds[index]).toISOString().substring(14, 21)}] ` : '';
-                return <div style={{paddingLeft: '1em'}}>{timestamp}{lyric}</div>
-            })}
+            <div>
+                {lyricLines.map((lyric, index) => {
+                    const timestamp = milliseconds[index] ? `[${new Date(0, 0, 0, 0, 0, 0, milliseconds[index]).toISOString().substring(14, 21)}] ` : '';
+                    return <div style={{paddingLeft: '1em'}}>{timestamp}{lyric}</div>
+                })}
+            </div>
         </section>
     );
 };
