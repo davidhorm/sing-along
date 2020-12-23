8f3c71aacd881ee964d67448cf2e28bc1f341a1b
davidhorm <davidhorm+github@gmail.com>
2020-12-23 14:14:11 -0800

    Remove basename from <HashRouter />

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <StrictMode>
-    <HashRouter basename="/sing-along">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
