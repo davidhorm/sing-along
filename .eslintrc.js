0b2bd918015432bbb781a6acf9fef4881d10edbf
David Horm <davidhorm@gmail.com>
2021-12-27 13:59:15 -0800

    Ignore some unused `import` eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,15 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    /**
+     * Ignore some unused `import` eslint rules:
+     * - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/extensions.md
+     * - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-unresolved.md
+     *   - prob should look at https://github.com/import-js/eslint-plugin-import/blob/main/README.md#resolvers
+     * - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/prefer-default-export.md
+     */
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
   },
 };
